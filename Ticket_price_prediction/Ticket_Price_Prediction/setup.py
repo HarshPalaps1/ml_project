e47ea09b9480c25facd77bb46c16e695e6e68c2c
Harsh pal <86770957+HarshPalaps1@users.noreply.github.com>
2024-05-19 23:29:55 +0530

    project basic structure files

---
+++ Ticket_price_prediction/Ticket_Price_Prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='For predicting the trip price by a taxes',
+    author='Harsh_Pal',
+    license='MIT',
+)
